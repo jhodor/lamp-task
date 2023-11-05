f519e63da7f7aa963a8b9c17100a34919d32d729
Jerzy Hodor <17220844+jhodor@users.noreply.github.com>
2023-11-04 22:40:26 -0500

    Cleanup and comments

--- web/api/adresses.php
+++ web/api/adresses.php
@@ -6,48 +6,56 @@ include('../lib/common.php');
 
 function getAllLocations(PDO $pdo): void
 {
-    $batchSize = 1000;
-
-    $stmt = $pdo->prepare("SELECT COUNT(*) FROM locations");
-    $stmt->execute();
-    $rowCount = $stmt->fetchColumn();
+    // Use paging to read db in chunks instead of single iterator to conserve memory
+    // As a result we echo array of rows, where each row is a JSON
 
     $page = 0;
-    $pageSize = $batchSize;
+    $pageSize = 1000;
 
-    // Send the JSON array start
-    echo '[';
-
-    while ($page * $pageSize < $rowCount) {
-        $query = "SELECT * FROM locations LIMIT :offset, :pageSize";
-        $offset = $page * $pageSize;
-
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
-        $stmt->bindParam(':pageSize', $pageSize, PDO::PARAM_INT);
+    try {
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM locations");
         $stmt->execute();
+        $rowCount = $stmt->fetchColumn();
 
-        $batch = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // Send the JSON array start
+        echo '[';
 
-        if (!empty($batch)) {
-            $i = 0;
-            foreach ($batch as $row) {
-                $json = json_encode($row);
-                if ($json) {
-                    echo $json;
-                }
-                $i++;
-                // Add a comma if there are more records coming
-                if ($page * $pageSize + $i < $rowCount) {
-                    echo ','.  PHP_EOL;
+        while ($page * $pageSize < $rowCount) {
+            // Read and send the contents of array
+            $query = "SELECT * FROM locations LIMIT :offset, :pageSize";
+            $offset = $page * $pageSize;
+
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->bindParam(':pageSize', $pageSize, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $batch = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if (!empty($batch)) {
+                $i = 0;
+                foreach ($batch as $row) {
+                    $json = json_encode($row);
+                    if ($json) {
+                        echo $json;
+                    }
+                    $i++;
+
+                    // Add a comma after JSON when more records are coming
+                    if ($page * $pageSize + $i < $rowCount) {
+                        echo ',' . PHP_EOL;
+                    }
                 }
             }
+            $page++;
         }
-        $page++;
-    }
 
-    // Send the JSON array end
-    echo ']';
+        // Send the JSON array end
+        echo ']';
+
+    } catch (Exception $e) {
+        echo json_encode(array('error' => $e->getMessage()));
+    }
 }
 
 // ****** MAIN ******* //
